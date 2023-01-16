0f0429bd9504562d83e6e055780d854fe134b7ce
ItsEthra <107059409+ItsEthra@users.noreply.github.com>
2023-01-16 20:10:26 +0300

    Added macros & examples crates

---
+++ examples/runner/src/main.rs
@@ -0,0 +1,5 @@
+const _PLUGIN: &[u8] = include_bytes!("../plugin.wasm");
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ runner.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cargo build --release --package plugin --target wasm32-unknown-unknown
