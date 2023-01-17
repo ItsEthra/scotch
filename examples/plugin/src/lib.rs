7e207d93f2acf5edb65cbf946380cb24c71a7831
ItsEthra <107059409+ItsEthra@users.noreply.github.com>
2023-01-17 16:30:57 +0300

    Added imports & exports

--- examples/plugin/src/lib.rs
+++ examples/plugin/src/lib.rs
@@ -1,1 +1,8 @@
+extern "C" {
+    fn get_number() -> i32;
+}
 
+#[no_mangle]
+extern "C" fn add_numbers(a: i32, b: i64) -> i64 {
+    unsafe { a as i64 + b + get_number() as i64 }
+}
--- examples/runner/src/main.rs
+++ examples/runner/src/main.rs
@@ -1,5 +1,28 @@
-const _PLUGIN: &[u8] = include_bytes!("../plugin.wasm");
+use eyre::Result;
+use wasmer::{imports, Function, Instance, Module, Store, TypedFunction};
 
-fn main() {
-    println!("Hello, world!");
+const PLUGIN: &[u8] = include_bytes!("../plugin.wasm");
+
+fn get_number() -> i32 {
+    15
+}
+
+fn main() -> Result<()> {
+    let mut store = Store::default();
+    let module = Module::from_binary(&store, PLUGIN)?;
+
+    let imports = imports! {
+        "env" => {
+            "get_number" => Function::new_typed(&mut store, get_number)
+        }
+    };
+    let instance = Instance::new(&mut store, &module, &imports)?;
+
+    let adder: TypedFunction<(i32, i64), i64> =
+        instance.exports.get_typed_function(&store, "add_numbers")?;
+
+    // 10 + 20 + 15 = 45
+    println!("Result: {}", adder.call(&mut store, 10, 20)?);
+
+    Ok(())
 }
