6c70d86fdeb1a2800aecc6d0d7c14372b3059cd6
ItsEthra <107059409+ItsEthra@users.noreply.github.com>
2023-01-21 17:56:30 +0300

    Improved example

--- examples/plugin/src/lib.rs
+++ examples/plugin/src/lib.rs
@@ -1,8 +1,20 @@
 scotch_guest::include_alloc!();
 
-use scotch_guest::guest_function;
+use scotch_guest::{guest_function, host_functions};
+
+#[host_functions]
+extern "C" {
+    fn print(val: &String);
+}
 
 #[guest_function]
 fn add_up_list(items: &Vec<i32>) -> i32 {
+    // Print numbers in reverse because why not.
+    items
+        .iter()
+        .rev()
+        .map(|num| format!("Hello number, {num}"))
+        .for_each(|text| print(&text));
+
     items.iter().sum::<i32>()
 }
--- examples/runner/src/main.rs
+++ examples/runner/src/main.rs
@@ -1,5 +1,5 @@
 use eyre::Result;
-use scotch_host::{guest_functions, make_exports, WasmPlugin};
+use scotch_host::{guest_functions, host_function, make_exports, make_imports, WasmPlugin};
 
 const PLUGIN_BYTES: &[u8] = include_bytes!("../plugin.wasm");
 
@@ -7,11 +7,17 @@ guest_functions! {
     pub add_up_list: fn(nums: &Vec<i32>) -> i32;
 }
 
+#[host_function]
+fn print(text: &String) {
+    println!("Wasm: {text}");
+}
+
 fn main() -> Result<()> {
     let plugin = WasmPlugin::builder()
-        .from_binary(PLUGIN_BYTES)?
-        .with_exports(make_exports!(add_up_list))
         .with_env(())
+        .from_binary(PLUGIN_BYTES)?
+        .with_imports(make_imports!(print))
+        .with_exports(make_exports!(add_up_list))
         .finish()?;
 
     let sum = plugin.function::<add_up_list>()(&vec![1, 2, 3, 4, 5])?;
